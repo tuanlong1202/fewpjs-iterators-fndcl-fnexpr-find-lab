b84c2a002c917370f043579849d412d926545c98
tuanlong1202 <tuanlong1202@gmail.com>
2021-04-04 22:22:14 -0400

    Array find lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+//
+function superbowlWin(arrRecord = []) {
+     var obj = arrRecord.find(winYear) ;
+     if (obj != null) {
+        return obj.year;
+     }
+}
 
+ function winYear(element) {
+    if (element.result == `W`) {
+       return element ;
+    }
+ }
